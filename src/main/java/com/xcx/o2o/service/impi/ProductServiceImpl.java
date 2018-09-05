94d24d3a0facf98cbe83bc1c0f7631d2e8bc20e6
XCX <JLU_Chenxi_Work@163.com>
2018-09-05 14:11:52 +0800

    06 05 2

--- src/main/java/com/xcx/o2o/service/impi/ProductServiceImpl.java
+++ src/main/java/com/xcx/o2o/service/impi/ProductServiceImpl.java
@@ -18,6 +18,7 @@ import com.xcx.o2o.enums.ProductStateEnum;
 import com.xcx.o2o.exceptions.ProductOperationException;
 import com.xcx.o2o.service.ProductService;
 import com.xcx.o2o.util.ImageUtil;
+import com.xcx.o2o.util.PageCalcultor;
 import com.xcx.o2o.util.PathUtil;
 
 @Service
@@ -26,46 +27,49 @@ public class ProductServiceImpl implements ProductService {
 	private ProductDao productDao;
 	@Autowired
 	private ProductImgDao productImgDao;
+
 	@Override
 	@Transactional
-	//1.处理缩略图,获取缩略图相对路径并赋值给product
-	//2.往tb_product写入商品信息,获取productId
-	//3.结合productId批量处理商品详情图
-	//4.将商品详情图列表批量插入tb_product_img中
+	// 1.处理缩略图,获取缩略图相对路径并赋值给product
+	// 2.往tb_product写入商品信息,获取productId
+	// 3.结合productId批量处理商品详情图
+	// 4.将商品详情图列表批量插入tb_product_img中
 	public ProductExecution addProduct(Product product, ImageHolder thumbnail, List<ImageHolder> productImgHolderList)
 			throws ProductOperationException {
 		// TODO Auto-generated method stub
-		if (product != null&&product.getShop() !=null && product.getShop().getShopId() !=null) {
-			//设置商品默认属性
+		if (product != null && product.getShop() != null && product.getShop().getShopId() != null) {
+			// 设置商品默认属性
 			product.setCreateTime(new Date());
 			product.setLastEditTime(new Date());
-			//默认为上架状态
+			// 默认为上架状态
 			product.setEnableStatus(1);
 			if (thumbnail != null) {
-				addThunbnail(product,thumbnail);
+				addThunbnail(product, thumbnail);
 			}
 			try {
-				//创建商品信息
+				// 创建商品信息
 				int effctedNum = productDao.insertProduct(product);
-				if(effctedNum <= 0) {
+				if (effctedNum <= 0) {
 					throw new ProductOperationException("创建商品失败");
 				}
 			} catch (ProductOperationException e) {
 				// TODO: handle exception
 				throw new ProductOperationException("创建商品失败:" + e.toString());
 			}
-			//若商品详情图不为空则添加
-			if(productImgHolderList != null && productImgHolderList.size() >0) {
-				addProductImgList(product,productImgHolderList);
+			// 若商品详情图不为空则添加
+			if (productImgHolderList != null && productImgHolderList.size() > 0) {
+				addProductImgList(product, productImgHolderList);
 			}
 			return new ProductExecution(ProductStateEnum.SUCCESS, product);
-		}else {
-			//传参为空则返回空值错误信息
+		} else {
+			// 传参为空则返回空值错误信息
 			return new ProductExecution(ProductStateEnum.EMPTY);
 		}
 	}
+
 	/**
 	 * 添加缩略图
+	 * 
 	 * @param product
 	 * @param thumbnail
 	 */
@@ -75,32 +79,34 @@ public class ProductServiceImpl implements ProductService {
 		String thunbnailAddr = ImageUtil.generateThumbnail(thumbnail, dest);
 		product.setImgAddr(thunbnailAddr);
 	}
+
 	/**
 	 * 批量添加图片
+	 * 
 	 * @param product
 	 * @param productImgList
 	 */
 	private void addProductImgList(Product product, List<ImageHolder> productImgHolderList) {
 		// TODO Auto-generated method stub
-		//获取图片存储路径,直接存放到相应店铺文件夹目录下
+		// 获取图片存储路径,直接存放到相应店铺文件夹目录下
 		String dest = PathUtil.getShopImagePath(product.getShop().getShopId());
 		List<ProductImg> productImgList = new ArrayList<ProductImg>();
-		//遍历图片一次去处理,并添加进productImg实体类里
-		for(ImageHolder productImgHolder:productImgHolderList) {
-			String imgAddr = ImageUtil.generateNormalImg(productImgHolder,dest);
+		// 遍历图片一次去处理,并添加进productImg实体类里
+		for (ImageHolder productImgHolder : productImgHolderList) {
+			String imgAddr = ImageUtil.generateNormalImg(productImgHolder, dest);
 			ProductImg productImg = new ProductImg();
 			productImg.setImgAddr(imgAddr);
 			productImg.setProductId(product.getProductId());
 			productImg.setCreateTime(new Date());
 			productImgList.add(productImg);
 		}
-		//如果确实是有图片需要添加,就执行批量添加操作
-		if (productImgList.size() > 0 ) {
+		// 如果确实是有图片需要添加,就执行批量添加操作
+		if (productImgList.size() > 0) {
 			try {
 				int effectedNum = productImgDao.batchInsertProductImg(productImgList);
 				if (effectedNum <= 0) {
 					throw new ProductOperationException("创建商品详情图片失败");
-					
+
 				}
 			} catch (Exception e) {
 				// TODO: handle exception
@@ -108,20 +114,21 @@ public class ProductServiceImpl implements ProductService {
 
 		}
 	}
+
 	@Override
 	@Transactional
-	//若缩略图参数有值,则处理缩略图,
-	//若原先存在缩略图则先删除再添加新图,之后获取缩略图相对路径并赋值给product
-	//若商品详情图列表参数有值,对商品详情图片列表进行同样的操作
-	//将tb_product_img下面的该商品原先的商品详情图记录全部清除
-	//更新tb_product的信息
-	public ProductExecution modifyProduct(Product product, ImageHolder thumbnail, List<ImageHolder> productImgHolderList)
-			throws ProductOperationException {
+	// 若缩略图参数有值,则处理缩略图,
+	// 若原先存在缩略图则先删除再添加新图,之后获取缩略图相对路径并赋值给product
+	// 若商品详情图列表参数有值,对商品详情图片列表进行同样的操作
+	// 将tb_product_img下面的该商品原先的商品详情图记录全部清除
+	// 更新tb_product的信息
+	public ProductExecution modifyProduct(Product product, ImageHolder thumbnail,
+			List<ImageHolder> productImgHolderList) throws ProductOperationException {
 		// TODO 自动生成的方法存根
-		if(product != null && product.getShop() != null && product.getShop().getShopId() != null) {
-			//给商品设置上默认属性
+		if (product != null && product.getShop() != null && product.getShop().getShopId() != null) {
+			// 给商品设置上默认属性
 			product.setLastEditTime(new Date());
-			//若商品缩略图不为空且原有缩略图不为空则删除原有缩略图并添加
+			// 若商品缩略图不为空且原有缩略图不为空则删除原有缩略图并添加
 			if (thumbnail != null) {
 				// 先获取一遍原有信息,因为原来的信息里有原图片地址
 				Product tempProduct = productDao.queryProductByProductId(product.getProductId());
@@ -130,47 +137,58 @@ public class ProductServiceImpl implements ProductService {
 				}
 				addThunbnail(product, thumbnail);
 			}
-			//如果有新存入商品详情图,则将原先的删除,并添加新的图片
+			// 如果有新存入商品详情图,则将原先的删除,并添加新的图片
 			if (productImgHolderList != null && productImgHolderList.size() > 0) {
 				deleteProductImgList(product.getProductId());
 				addProductImgList(product, productImgHolderList);
 			}
 			try {
-				//更新商品信息
+				// 更新商品信息
 				int effectedNum = productDao.updateProduct(product);
 				if (effectedNum <= 0) {
 					throw new ProductOperationException("更新商品信息失败");
 				}
-				return new ProductExecution(ProductStateEnum.SUCCESS,product);
+				return new ProductExecution(ProductStateEnum.SUCCESS, product);
 			} catch (Exception e) {
 				// TODO: handle exception
-				throw new ProductOperationException("更新商品信息失败"+e.toString());
+				throw new ProductOperationException("更新商品信息失败" + e.toString());
 			}
-		}else {
+		} else {
 			return new ProductExecution(ProductStateEnum.EMPTY);
 		}
 	}
+
 	private void deleteProductImgList(Long productId) {
 		// TODO 自动生成的方法存根
-		//根据productId获取原来的图片
+		// 根据productId获取原来的图片
 		List<ProductImg> productImgList = productImgDao.queryProductImgList(productId);
-		//干掉原来的图片
+		// 干掉原来的图片
 		for (ProductImg productImg : productImgList) {
 			ImageUtil.deleteFileOrPath(productImg.getImgAddr());
-			
+
 		}
-		//删除数据库里原有图片的信息
+		// 删除数据库里原有图片的信息
 		productImgDao.deleteProductImgByProductId(productId);
 	}
+
 	@Override
 	public ProductExecution getProductList(Product productCondition, int pageIndex, int pageSize) {
 		// TODO 自动生成的方法存根
-		return null;
+		// 页码转换成数据库的行码,并调用dao层取回指定页码的商品列表
+		int rowIndex = PageCalcultor.calculateRowIndex(pageIndex, pageSize);
+		List<Product> productList = productDao.queryProductList(productCondition, rowIndex, pageSize);
+		// 基于同样的查询条件返回该查询条件下的商品总数
+		int count = productDao.queryProductCount(productCondition);
+		ProductExecution pe = new ProductExecution();
+		pe.setProductList(productList);
+		pe.setCount(count);
+		return pe;
 	}
+
 	@Override
 	public Product getProductById(long productId) {
 		// TODO 自动生成的方法存根
 		return productDao.queryProductById(productId);
 	}
-	
+
 }
